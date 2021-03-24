af5a81c4bb07219510ceb8692a07211e6ce488d7
syl20ko <sylvain.caron@baliztic.com>
2021-03-24 16:42:34 +0100

    galaxy feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React, { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+import React, { Suspense, useRef, useMemo } from "react";
+import { Canvas , useFrame } from "react-three-fiber";
+import { OrbitControls} from "@react-three/drei";
 import Police from "./Police";
 import Mars from "./Mars";
+
+const Stars = () => {
+  let group = useRef()
+  let theta = 0
+  useFrame(() => {
+    // Some things maybe shouldn't be declarative, we're in the render-loop here with full access to the instance
+    const r = 2 * Math.sin(THREE.Math.degToRad((theta += 0.1)))
+    const s = Math.cos(THREE.Math.degToRad(theta * 1))
+    group.current.rotation.set(r, r, r)
+    group.current.scale.set(s, s, s)
+  })
+  const [geo, mat, coords] = useMemo(() => {
+    const geo = new THREE.SphereBufferGeometry(1, 10, 10)
+    const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
+    const coords = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
+    return [geo, mat, coords]
+  }, [])
+  return (
+    <group ref={group}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      ))}
+    </group>
+  )
+}
 
 function App() {
   return (
@@ -13,19 +39,14 @@ function App() {
         <Mars />
         <group>
           <Police />
-          <Stars
-            distance={600000} // Camera distance (default=450000)
-            sunPosition={[0, 1, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
-            inclination={0} // Sun elevation angle from 0 to 1 (default=0)
-            azimuth={0.25} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
-          />
+          <Stars />
         </group>
       </Suspense>
       <OrbitControls
         maxPolarAngle={Math.PI / 2}
         minDistance={[35]}
         minPolarAngle={0}
-        autoRotate
+        /* autoRotate */
       />
     </Canvas>
   );
